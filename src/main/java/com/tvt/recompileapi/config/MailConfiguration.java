9976d5ad4b50b90902edf5acde229c13e7705c56
vanthinh <van.thinh.tran@dksh.com>
2024-03-05 15:48:30 +0700

    Remove debug email

--- src/main/java/com/tvt/recompileapi/config/MailConfiguration.java
+++ src/main/java/com/tvt/recompileapi/config/MailConfiguration.java
@@ -33,7 +33,7 @@ public class MailConfiguration {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        props.put("mail.debug", "false");
 
         return mailSender;
     }
